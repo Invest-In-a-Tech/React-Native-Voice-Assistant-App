3bf31f4e64989df72c84369325f6e20ff213012f
AdeptistJune <128441472+AdeptistJune@users.noreply.github.com>
2023-04-26 16:52:24 +0000

    deleted gunicorn_confiy.py

--- backend/main.py
+++ backend/main.py
@@ -73,6 +73,6 @@ def transcribe():
     return jsonify({'userInput': user_input, 'assistantResponse': system_message, 'audioURI': audio_base64})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
     # app.run()
     
